899fe5a813fed967d1be1032f75dec04fff20cee
Jaheda Sultana <jahedasultana47@gmail.com>
2025-07-05 20:47:59 +0600

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { Application, Request, Response } from "express";
 import { bookRoutes } from "./app/controllers/book.controller";
 import borrowRoutes from "./app/controllers/borrow.controller";
-
+import cors from "cors";
 
 const app: Application = express();
 app.use(express.json());
+
+app.use(cors({
+  origin:["https://assinment-4-six.vercel.app",
+     "http://localhost:5173"]
+}));
 
 app.use("/api/books", bookRoutes);
 app.use("/api/borrow", borrowRoutes);
